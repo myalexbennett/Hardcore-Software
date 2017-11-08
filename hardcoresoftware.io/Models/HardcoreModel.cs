64a888bc61470be9114fae9c8759c0a7b00d06de
Alex Bennett <myalexbennett@yahoo.com>
2017-11-07 19:16:51 -0500

    first commit

---
+++ hardcoresoftware.io/Models/HardcoreModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hardcoresoftware.io.Models
+{
+    public class HardcoreModel
+    {
+    }
+}
